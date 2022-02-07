1e12a0dd8158dba3144c8232b444bff959402cac
lovishgupta1001@gmail.com <93213511+Lovishgupta1001@users.noreply.github.com>
2022-02-08 00:54:46 +0530

    Update gcd_by_euclid.cpp

--- gcd_by_euclid.cpp
+++ gcd_by_euclid.cpp
@@ -1,18 +1,33 @@
 #include<iostream>
 using namespace std;
-int fgcd(int,int);
+int fgcd1(int,int);
+int fgcd2(int,int);
 int main()
 {
    int a,b;
    cout<<"Enter Two No.: ";
    cin>>a>>b;
-   cout<<"GCD of "<<a<<" & "<<b<<" is: "<<fgcd(a,b);
+   cout<<"GCD of "<<a<<" & "<<b<<" is: "<<fgcd1(a,b)<<endl;
+   cout<<"GCD of "<<a<<" & "<<b<<" is: "<<fgcd2(a,b);
    return 0;
 }
-int fgcd(int a,int b)
+//Approach 1 which take more time
+int fgcd1(int a,int b)
+{
+   while(a!=b)
+   {
+      if(a>b)
+      a=a-b;
+      else
+      b=b-a;
+   }
+   return a;
+}
+//Approach 2 which take less time in execution than Approach 1
+int fgcd2(int a,int b)
 {
    if(b==0)
    return a;
    else
-   return fgcd(b,a%b);
+   return fgcd2(b,a%b);
 }
